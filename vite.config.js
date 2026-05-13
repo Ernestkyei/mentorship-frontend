f11963d97c607003b2f977eb41fe1b470924e1f9
ErnestKyei <kyeiernest86@gmail.com>
2026-05-13 20:08:36 +0000

    donWithChanges

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mentorship-frontend/',
+  base: './',
   css: {
     postcss: './postcss.config.js',
   },
